3cd21458d693b40c8216d10348c1bbd4b20ed582
kirger06 <kirger06@gmail.com>
2026-05-13 15:31:31 +0300

    create ApiRoutes

--- RieltorCRM/RieltorCRM/obj/Debug/net8.0/RieltorCRM.AssemblyInfo.cs
+++ RieltorCRM/RieltorCRM/obj/Debug/net8.0/RieltorCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e15ec4d39f6f8a1b6e0697ec62e52a4eb9e79c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de26a1ae66808aa9700b2e7e0ab024bdcab0dcc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
