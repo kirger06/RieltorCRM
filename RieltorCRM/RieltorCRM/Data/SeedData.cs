26e15ec4d39f6f8a1b6e0697ec62e52a4eb9e79c
kirger06 <kirger06@gmail.com>
2026-05-13 15:25:43 +0300

    create SeedData

---
+++ RieltorCRM/RieltorCRM/Data/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RieltorCRM.Models;
+
+
+namespace RieltorCRM.Data
+{
+    public static class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            string[] roles = { "Admin", "Agent", "Client", "Seller", "OfficeManager", "Accountant" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole<int> { Name = role });
+                }
+            }
+
+            var testUsers = new[]
+            {
+                new { Email = "admin@rioter.ru", FirstName = "Администратор", LastName = "Системы", Role = UserRole.Admin, Password = "Admin123!" },
+                new { Email = "agent@rioter.ru", FirstName = "Иван", LastName = "Риелторов", Role = UserRole.Agent, Password = "Agent123!" },
+                new { Email = "client@rioter.ru", FirstName = "Петр", LastName = "Клиентов", Role = UserRole.Client, Password = "Client123!" },
+                new { Email = "seller@rioter.ru", FirstName = "Мария", LastName = "Продавцова", Role = UserRole.Seller, Password = "Seller123!" },
+                new { Email = "manager@rioter.ru", FirstName = "Ольга", LastName = "Офисная", Role = UserRole.OfficeManager, Password = "Manager123!" },
+                new { Email = "buh@rioter.ru", FirstName = "Елена", LastName = "Бухгалтерова", Role = UserRole.Accountant, Password = "Buh123!" }
+            };
+
+            foreach (var testUser in testUsers)
+            {
+                var existingUser = await userManager.FindByEmailAsync(testUser.Email);
+                if (existingUser == null)
+                {
+                    var user = new User
+                    {
+                        UserName = testUser.Email,
+                        Email = testUser.Email,
+                        FirstName = testUser.FirstName,
+                        LastName = testUser.LastName,
+                        Role = testUser.Role,
+                        IsActive = true,
+                        CreatedAt = DateTime.UtcNow,
+                        EmailConfirmed = true
+                    };
+
+                    var result = await userManager.CreateAsync(user, testUser.Password);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(user, testUser.Role.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
--- RieltorCRM/RieltorCRM/obj/Debug/net8.0/RieltorCRM.AssemblyInfo.cs
+++ RieltorCRM/RieltorCRM/obj/Debug/net8.0/RieltorCRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab06d94f92730e2b9278c79c2e32988a782dcdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604da88fc1c051602a7d3df08770821debd248bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RieltorCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
